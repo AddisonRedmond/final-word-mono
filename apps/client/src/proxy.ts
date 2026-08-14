5891171945b9da7bbe7ded25ac57c4cee523918f
addison <addison@addisonredmond.com>
2026-08-13 19:35:45 -0500

    forgot to save

--- apps/client/src/proxy.ts
+++ apps/client/src/proxy.ts
@@ -2,7 +2,6 @@ import { type NextRequest } from "next/server";
 import { updateSession } from "@/utils/supabase/proxy";
 
 export async function proxy(request: NextRequest) {
-  console.log("TEST");
   return await updateSession(request);
 }
 
