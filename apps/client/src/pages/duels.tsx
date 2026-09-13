640d0726a0a9ad1985dbea4964272fd536982cb2
addison <addison@addisonredmond.com>
2026-09-13 11:02:02 -0500

    adding duel UI components

--- apps/client/src/pages/duels.tsx
+++ apps/client/src/pages/duels.tsx
@@ -1,13 +1,28 @@
 import Navbar from "@/components/navigation/navbar";
+import Tile from "@/components/tile";
 import { api } from "@/utils/api";
 
 const Duels = () => {
-  const allFriends = api.friends.list.useQuery();
+  const { data, isLoading, error } = api.friends.list.useQuery();
 
   return (
-    <div>
+    <div className="h-screen flex items-center flex-col gap-y-2 py-5">
+      {/* TODO: add navbar to the app, not individual pages */}
       <Navbar />
-      <p>DUELS</p>
+      <Tile word="DUEL" revealed={true} size="md" variant="correct" />
+      <div className="flex flex-col items-center justify-center grow">
+        <div className=" w-2xl h-10/12 outline outline-stone-200 bg-white rounded-md shadow-lg p-2">
+          <div>{/* same collapsable search input from friends */}</div>
+          <div>{/* Scrollable friends list */}</div>
+        </div>
+      </div>
+
+      <div className="flex flex-col items-center justify-center grow">
+        <div className=" w-2xl h-full outline outline-stone-200 bg-white rounded-md shadow-lg p-2">
+          <div>{/* same collapsable search input from friends */}</div>
+          <div>{/* Scrollable friends list */}</div>
+        </div>
+      </div>
     </div>
   );
 };
